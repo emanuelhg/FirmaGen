848ce0e9ff6a0ac7141b2bf149697fdc2415e1b1
Emanuel Guzzetti <69657719+emanuelhg@users.noreply.github.com>
2023-04-09 12:29:32 -0300

    Optimización de JS y estilos

--- scripts.js
+++ scripts.js
@@ -1,41 +1,33 @@
 // Datos del formulario:
+// Asigna los valores de los campos de entrada a los elementos de salida correspondientes.
+// Si un campo está vacío, se asigna un valor predeterminado.
 
-$('#nombreIn').on('input', (e) => {
-  const nombre = $(e.target).val() === '' ? 'Nombre y Apellido' : $(e.target).val();
-  $("#nombreOut").html(nombre);
-});
+function manejarEntrada(idEntrada, idSalida, textoPredeterminado) {
+  const entrada = $(`#${idEntrada}`).val() === '' ? textoPredeterminado : $(`#${idEntrada}`).val();
+  $(`#${idSalida}`).html(entrada);
+}
 
-$('#sectorIn').on('input', (e) => {
-  const sector = $(e.target).val() === '' ? 'Empresa o Sector' : $(e.target).val();
-  $("#sectorOut").html(sector);
-});
+$('#nombreIn').on('input', () => manejarEntrada('nombreIn', 'nombreOut', 'Nombre y Apellido'));
 
-$('#dirIn').on('input', (e) => {
-  const dir = $(e.target).val() === '' ? 'Dirección (CP). Localidad - Provincia.' : $(e.target).val();
-  $("#dirOut").html(dir);
-});
+$('#sectorIn').on('input', () => manejarEntrada('sectorIn', 'sectorOut', 'Empresa o Sector'));
 
+$('#dirIn').on('input', () => manejarEntrada('dirIn', 'dirOut', 'Dirección (CP). Localidad - Provincia.'));
 
-$('#telIn').on('input', (e) => {
-  const tel = $(e.target).val() === '' ? '(Caract) Número de Tel + Interno' : $(e.target).val();
-  $("#telOut").html(tel);
-});
+$('#telIn').on('input', () => manejarEntrada('telIn', 'telOut', '(Caract) Número de Tel + Interno'));
 
-// Captura de datos de formulario y generación de PNG:
+// Captura los datos de formulario y genera un archivo PNG con la imagen de la firma:
 
-const $boton = document.querySelector("#btnCapturar"), // Selecciona el botón.
-$objetivo = document.querySelector("#firmaCaptura"); //  Selecciona la tabla que contiene la firma.
+const $boton = document.querySelector("#btnCapturar"), // Selecciona el botón de captura.
+  $objetivo = document.querySelector("#firmaCaptura"); // Selecciona la tabla que contiene la firma.
 
-// Agrega event listener al botón:
+// Agrega un event listener al botón de captura:
 
 $boton.addEventListener("click", () => {
-  html2canvas($objetivo) // Llama a la librería html2canvas para capturar la firma.
-    .then(canvas => {
-      // Crea un elemento <a>
-      let enlace = document.createElement('a');
-      enlace.download = `firma - ${document.querySelector("#nombreOut").textContent}.png`; //nombre del archivo.
-      // Convierte la imagen a Base64.
-      enlace.href = canvas.toDataURL();
-      enlace.click();
-    });
+  html2canvas($objetivo).then(canvas => { // Usa la librería html2canvas para capturar la firma.
+    let enlace = document.createElement('a'); // Crea un elemento <a> para descargar la imagen como un archivo PNG.
+    const nombreArchivo = `firma - ${($("#nombreOut").text())}.png`; // Asigna un nombre al archivo.
+    enlace.download = nombreArchivo;
+    enlace.href = canvas.toDataURL(); // Convierte la imagen a Base64.
+    enlace.click(); // Descarga la imagen como un archivo PNG.
+  }); 
 });
